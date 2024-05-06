0b75b0f9acb77d2d75314471deb203b672d13134
ayushrakesh <ayushrakesh35711artlover@gmail.com>
2024-05-06 20:35:19 +0530

    learning loops ,logical operators

--- error-handling/main.go
+++ error-handling/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func sendMsg(msg string) (string, error) {
 	if len(msg) <= 8 {
@@ -9,11 +12,45 @@ func sendMsg(msg string) (string, error) {
 		return "Error", fmt.Errorf("Something went wrong!")
 	}
 }
+func divide(dividend, divisor float32) (float32, error) {
+	if divisor == 0 {
+
+		// standard library package function -> errors.New()
+		return 0.0, errors.New("no divide by zero")
+	}
+	return dividend / divisor, nil
+}
+func calculateMaxMessages() float64 {
+	thre := 100
+
+	// maxMessages:=0
+	totalCost := 0.0
+
+	for i := 0; ; i++ {
+		totalCost = totalCost + (1.0 + float64(i)*0.01)
+
+		if totalCost > float64(thre) {
+			return float64(i)
+		}
+	}
+}
+func understandingLoops() {
+
+	maxMessages := calculateMaxMessages()
+	fmt.Println(maxMessages)
+}
 func main() {
 	msg, err := sendMsg("this is")
 
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(msg)
 	}
-	fmt.Println(msg)
+
+	// fmt.Println(msg)
+
+	fmt.Println(divide(5, 7))
+
+	understandingLoops()
 }
