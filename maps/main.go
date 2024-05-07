4d6e5d03c4ea991b06d02230dcbf69bc37f37e2f
ayushrakesh <ayushrakesh35711artlover@gmail.com>
2024-05-07 17:43:50 +0530

    maps

---
+++ maps/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type user struct {
+	name        string
+	number      int
+	isScheduled bool
+}
+
+func createUserMap(names []string, phones []int) (map[string]int, error) {
+	m := map[string]int{}
+
+	// fmt.Println(m)
+	// fmt.Println(len(m))
+
+	if len(names) != len(phones) {
+		return nil, errors.New("invalid sizes!")
+	} else {
+		for i := 0; i < len(names); i++ {
+			m[names[i]] = phones[i]
+		}
+	}
+	return m, nil
+}
+func deleteIfNecessary(userMap map[string]user, name string) (bool, error) {
+
+	user, ok := userMap[name]
+	if !ok {
+		return false, errors.New("not found")
+	} else {
+		// user := userMap[name]
+
+		if user.isScheduled {
+			delete(userMap, name)
+			return true, nil
+		} else {
+			return false, nil
+		}
+	}
+}
+func main() {
+	// m := map[string]int{
+	// 	"john":     1,
+	// 	"vicky":    2,
+	// 	"steve":    3,
+	// 	"mitchell": 4,
+	// }
+
+	names := []string{"john", "vicky", "mitchell", "steve"}
+	phones := []int{101, 102, 103}
+
+	m, err := createUserMap(names, phones)
+
+	// var n map[string]int
+	n := make(map[string]int) //nitilise empty array
+	fmt.Println(m, err)
+	fmt.Println(n)
+
+	fmt.Println("-------------")
+
+	userMap := map[string]user{
+		"john": {
+			name: "john", number: 123, isScheduled: true,
+		},
+		"vicky": {
+			name:   "vicky",
+			number: 124, isScheduled: false,
+		},
+		"steve": {
+			name: "steve", number: 125, isScheduled: true,
+		},
+		"mitchell": {
+			name: "mitchell", number: 126, isScheduled: true,
+		},
+	}
+
+	isdeleted, err := deleteIfNecessary(userMap, "johni")
+
+	fmt.Println(isdeleted, err, userMap)
+
+}
