13f6380b4949627b29927a372323b7f51a7f088c
ayushrakesh <ayushrakesh35711artlover@gmail.com>
2024-05-13 09:25:38 +0530

    Channels

--- channels/main.go
+++ channels/main.go
@@ -1,5 +1,62 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+type email struct {
+	t time.Time
+}
+
+func checkTime(emails []email) {
+	ch := make(chan bool)
+
+	go func() {
+		for _, em := range emails {
+			if em.t.Before(time.Date(2024, 1, 2, 12, 0, 0, 0, time.Local)) {
+				ch <- true
+				continue
+			} else {
+				ch <- false
+			}
+		}
+	}()
+
+	// for i := 0; i < len(emails); i++ {
+	v := <-ch
+	fmt.Println(v)
+
+	v = <-ch
+	fmt.Println(v)
+	v = <-ch
+	fmt.Println(v)
+	v = <-ch
+	fmt.Println(v)
+	// }
+
+}
+func main() {
+	// e := email{
+	// 	t: time.Date(2024, 1, 1, 12, 0, 0, 0, time.Local),
+	// }
+
+	// fmt.Println(e.t)
+
+	emails := []email{
+		{
+			t: time.Date(2024, 1, 1, 12, 0, 0, 0, time.Local),
+		},
+		{
+			t: time.Date(2024, 1, 2, 12, 0, 0, 0, time.Local),
+		},
+		{
+			t: time.Date(2024, 1, 2, 12, 0, 0, 0, time.Local),
+		},
+		{
+			t: time.Date(2024, 1, 2, 12, 0, 0, 0, time.Local),
+		},
+	}
+
+	checkTime(emails)
 }
